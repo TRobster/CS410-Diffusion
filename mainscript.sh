41aa14830387247d1d7182ad6f92f4931aa1574a
Trevor Robbins <trevorro@login3.talapas.uoregon.edu>
2026-05-13 16:10:33 -0700

    ds with '#' will be ignored, and an empty message aborts the commit. First file

---
+++ mainscript.sh
@@ -0,0 +1,22 @@
+
+
+#!/bin/bash
+#SBATCH --job-name=my_cuda_job
+#SBATCH --partition=gpu
+#SBATCH --account=smearlab
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=64G
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=gpu-80gb,no-mig
+#SBATCH --cpus-per-task=8
+#SBATCH --output=./out_cuda_%j
+#SBATCH --error=./errors_cuda_%j
+
+# Load the modules instead of conda
+module load gcc
+module load cuda/13.0
+
+# Compile the .cu file with nvcc
+nvcc -O2 -o my_program /projects/smearlab/trevorro/gputesting/learn.cu
+# Run the compiled binary
+./my_program
