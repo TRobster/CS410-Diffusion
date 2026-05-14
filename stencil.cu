9e0b2699ef75cf1f570ddc91983d9866185e9743
TRobster <trevorro@uoregon.edu>
2026-05-14 14:50:09 -0700

    bug

--- stencil.cu
+++ stencil.cu
@@ -8,7 +8,7 @@ __global__ void memTest(int *x, int size) // Pass CPU integer as size. #FIX 2
 {
     int workID = threadIdx.x + (blockDim.x * blockIdx.x);
     
-    x[workID] = (workID < (FIXED / 2));
+    x[workID] = (workID < (size / 2));
 }
 
 int main() 
