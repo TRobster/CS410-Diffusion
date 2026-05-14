94859ca867669d74ea3348a0663ead99ddd5b1bb
TRobster <trevorro@uoregon.edu>
2026-05-14 13:17:47 -0700

    Removed some discared files

--- stencil.cu
+++ stencil.cu
@@ -3,8 +3,8 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
 
-static int FIXED = 32;
-__global__ void memTest(int *x)
+static int FIXED = 32; // << -- STATIC Int is only accessible on CPU! Not GPU. #FIX 2
+__global__ void memTest(int *x, int size) // Pass CPU integer as size. #FIX 2
 {
     int workID = threadIdx.x + (blockDim.x * blockIdx.x);
     
@@ -15,13 +15,13 @@ int main()
 {
     // Host memory allocating 
     int *host;
-    host = malloc(host, sizeof(int) * FIXED);
+    host = (int*)malloc(sizeof(int) * FIXED); // << --- MALLOC returns pointer #FIX 1
     // Device memory allocating
     int *dev; 
-    dev = cudaMalloc(&dev, sizeof(int) * FIXED);
+    cudaMalloc(&dev, sizeof(int) * FIXED); // << --- cudaMALLOC returns error code #FIX 1
     cudaMemcpy(dev, host, sizeof(int) * FIXED, cudaMemcpyHostToDevice);
 
-    memTest<<<1, 32>>>(dev);
+    memTest<<<1, 32>>>(dev, FIXED);
 
     cudaMemcpy(host, dev, sizeof(int) * FIXED, cudaMemcpyDeviceToHost);
 
